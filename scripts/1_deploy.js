8e183ba9109180d323073c685c6ae8e39964e09f
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-06-06 06:04:05 -0700

    Update deployment script

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -8,15 +8,27 @@ const hre = require('hardhat')
 
 async function main() {
   const MAX_SUPPLY = 2000000
+  const decimals = 18
+  const quorum = 0.25
 
   // Deploy Token
   const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(MAX_SUPPLY)
-
+  let token = await Token.deploy(hre.ethers.utils.parseUnits(MAX_SUPPLY.toString(), decimals));
   await token.deployed()
+
   console.log(`Token deployed to: ${token.address}`)
   console.log(`Token supply: ${(await token.totalSupply())}`)
   console.log(`Token decimals: ${(await token.decimals())}\n`)
+  const [deployer] = await hre.ethers.getSigners()
+  console.log(`Deployer balance: ${(await token.balanceOf(deployer.address))}\n`)
+
+  // Deploy DelegatedDAO
+  const DelegatedDAO = await hre.ethers.getContractFactory('DelegatedDAO')
+  let delegatedDAO = await DelegatedDAO.deploy(token.address, hre.ethers.utils.parseUnits((quorum*MAX_SUPPLY).toString(), decimals));
+  await delegatedDAO.deployed()
+
+  console.log(`DelegatedDAO deployed to: ${delegatedDAO.address}`)
+  console.log(`DelegatedDAO quorum: ${(await delegatedDAO.quorum())}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
